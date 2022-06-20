7cefaa0c7c87ea41ce8cd58a2a224333a37e7a75
Jarett Dewbury <jdewbury@uwaterloo.ca>
2022-06-20 15:04:03 -0400

    updated as_json

--- fhirclient/models/fhirabstractbase.py
+++ fhirclient/models/fhirabstractbase.py
@@ -289,7 +289,6 @@ class FHIRAbstractBase(object):
                 else:
                     try:
                         found.add(of_many or jsname)
-                        print(type(value))
                         js[jsname] = value.as_json(require_non_optional=require_non_optional) if hasattr(value, 'as_json') else value
                     except FHIRValidationError as e:
                         err = e.prefixed(name)
